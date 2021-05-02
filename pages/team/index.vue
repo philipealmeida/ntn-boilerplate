2139b8b7f1dd95e1c51be591763ac84cda983ac2
Philipe Almeida <devpalmeida@gmail.com>
2021-05-01 21:27:53 -0300

    collection - new collection test page

---
+++ pages/team/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <section v-if="team" class="w-full max-w-5xl mx-auto">
+      <h1 class="title">Team</h1>
+      <posts post-type="team" :amount="10" />
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    let posts
+    try {
+      posts = await $content('team').fetch()
+    } catch (e) {
+      error({ message: 'Team posts not found' })
+    }
+    return { posts }
+  },
+}
+</script>
